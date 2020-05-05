a7a170dfa3bdcddfd282d9d0fe0337aa2999fa1d
PuneetSingla1234 <puneet2singla@gmail.com>
2020-05-05 10:11:32 +0530

    Refactor Services.

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/PetService.java
@@ -2,11 +2,7 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Pet;
 
-import java.util.Set;
 
-public interface PetService {
-    Pet findById(Long id);
+public interface PetService extends CrudService<Pet,Long>{
 
-    Pet save(Pet owner);
-    Set<Pet> findAll();
 }
