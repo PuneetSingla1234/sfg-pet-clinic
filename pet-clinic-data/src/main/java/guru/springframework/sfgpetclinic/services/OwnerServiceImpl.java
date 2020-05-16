b3d67dc8d6a7a5b597dde81cd6f433b2197af576
PuneetSingla1234 <puneet2singla@gmail.com>
2020-05-16 13:41:48 +0530

    Modified Services and Added Bootstrap DataLoader

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerServiceImpl.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/OwnerServiceImpl.java
@@ -2,9 +2,14 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Owner;
 
-public class OwnerServiceImpl extends AbstractMapService<Owner,Long> implements CrudService<Owner,Long>{
+public class OwnerServiceImpl extends AbstractMapService<Owner,Long> implements OwnerService{
     @Override
     public Owner save(Owner object) {
         return super.save(object.getId(),object);
+    }
+
+    @Override
+    public Owner findByLastName(String lastName) {
+        return null;
     }
 }
--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetServiceImpl.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/VetServiceImpl.java
@@ -2,7 +2,7 @@ package guru.springframework.sfgpetclinic.services;
 
 import guru.springframework.sfgpetclinic.model.Vet;
 
-public class VetServiceImpl extends AbstractMapService<Vet,Long> implements CrudService<Vet,Long> {
+public class VetServiceImpl extends AbstractMapService<Vet,Long> implements VetService {
     @Override
     public Vet save(Vet object) {
         return super.save(object.getId(),object);
