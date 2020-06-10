6d1e3faa67d743ee4fd550afb23617644dce20f1
PuneetSingla1234 <puneet2singla@gmail.com>
2020-06-10 23:07:22 +0530

    Implemented JPA Visit Service, loaded new data

--- pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitServiceMap.java
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/map/VisitServiceMap.java
@@ -2,7 +2,9 @@ package guru.springframework.sfgpetclinic.services.map;
 
 import guru.springframework.sfgpetclinic.model.Visit;
 import guru.springframework.sfgpetclinic.services.VisitService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class VisitServiceMap extends AbstractMapService<Visit,Long> implements VisitService {
     @Override
     public Visit save(Visit object){
---
+++ pet-clinic-data/src/main/java/guru/springframework/sfgpetclinic/services/springdatajpa/VisitServiceJpa.java
@@ -0,0 +1,10 @@
+package guru.springframework.sfgpetclinic.services.springdatajpa;
+
+import org.springframework.context.annotation.Profile;
+import org.springframework.stereotype.Service;
+
+@Service
+@Profile("servicedatajpa")
+public class VisitServiceJpa {
+
+}
